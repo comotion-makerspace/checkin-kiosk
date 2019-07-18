214a8a953bb68a696e7534b5fe06a4f6cc5395ee
codycodes <codycodes@users.noreply.github.com>
2019-07-18 14:35:17 -0700

    :sparkles: add cron scheduler for UW Groups staff

--- src/staff/main.py
+++ src/staff/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_apscheduler import APScheduler
 import requests
 import socketio
 import pickle
@@ -7,39 +8,51 @@ import os
 import sys
 import logging
 
+GROUP = os.environ['GROUPS_GROUP']
 GROUPS_CERT = '.{}{}'.format(os.sep, os.environ['GROUPS_CERT'])
 GROUPS_KEY = '.{}{}'.format(os.sep, os.environ['GROUPS_KEY'])
 GROUPS_ROOT = '.{}{}'.format(os.sep, os.environ['GROUPS_CERT_ROOT'])
-GROUP = os.environ['GROUPS_GROUP']
 
 app = Flask(__name__)
+# TODO: make all envvars passed via docker-compose
+app.config.from_envvar('APP_SETTINGS_STAFF')
+
+scheduler = APScheduler()
 
 def staff_checkin(netid):
     ''' Determines what to do when a staff is checking in '''
-    # TODO: determine what things to do...
+    # TODO: add hooks for test MSFT Teams bot here
     return '{} -> staff'.format(netid)
 
 @app.route("/staff", methods=["POST"])
 def is_staff_member(**kwargs):
     ''' Determines if the netid matches a netid in the list of staff '''
-    netid = kwargs.get('netid', None)
-    if not netid:
-        netid = request.get_json()['data']
-    members = None
     try:
-        with open('data.pickle', 'rb') as f:
-            members = pickle.load(f)
-    except (OSError, IOError) as e:
+        # ! test if token check is working...
+        # ! setup requirements.txt to be correct
+        if request.args.get('token') == app.config['FLASK_TOKEN']:
+            # get netid from request or previous call
+            netid = kwargs.get('netid', None)
+            if not netid:
+                netid = request.get_json()['data']
+            members = None
+            try:
+                with open('data.pickle', 'rb') as f:
+                    members = pickle.load(f)
+            except (OSError, IOError) as e:
+                print(e)
+                get_staff_members()
+                is_staff_member(netid=netid)
+            if members:
+                if netid in \
+                    [ m['id'] for m in members['data'] if m['type'] == 'uwnetid' ]:
+                        return staff_checkin(netid)
+                else:
+                    return '{} -> not staff'.format(netid)
+    except Exception as e:
         print(e)
-        get_staff_members()
-        is_staff_member(netid=netid)
-    if members:
-        if netid in \
-            [ m['id'] for m in members['data'] if m['type'] == 'uwnetid' ]:
-                return staff_checkin(netid)
-        else:
-            return '{} -> not staff'.format(netid)
 
+@scheduler.task('cron', id='get_staff_members', day='*', hour='4')
 def get_staff_members():
     try:
         with requests.Session() as s:
@@ -47,16 +60,24 @@ def get_staff_members():
             s.verify = GROUPS_ROOT
             r = s.get('https://iam-ws.u.washington.edu:7443/group_sws/v3/group/{}/effective_member'
                   .format(GROUP))
+            fmt_time=time.strftime("%m-%d-%Y @ %H:%M%p")
             if r.status_code == 200:
+                print('{}, successfully fetched staff from UW Groups'
+                      .format(fmt_time))
                 with open('data.pickle', 'wb') as f:
                     pickle.dump(r.json(), f, pickle.HIGHEST_PROTOCOL)
-                # logging.info('Fetching Groups\' staff members')
-                print('Fetching Groups\' staff members')
             else:
-                print('ERROR, could not get Groups\' members')
+                print('{}, ERROR: could not fetch staff from UW Groups'
+                      '\nPlease restart this container by turning the computer'
+                      ' off and on again'
+                      '\nAlso ensure there is an Internet connection &'
+                      ' UW Groups API is up'
+                      .format(fmt_time))
                 sys.exit(1)
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run(host='0.0.0.0', port=app.config['FLASK_PORT_STAFF'])
