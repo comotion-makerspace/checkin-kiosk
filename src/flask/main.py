63794e68cf1128a203996e97bd08bcf03ee15b09
codycodes <codycodes@users.noreply.github.com>
2019-07-18 01:48:36 -0700

    :art: unnecessary comments

--- src/flask/main.py
+++ src/flask/main.py
@@ -16,8 +16,7 @@ def parse_activity_webhook(request):
     try:
         if request.args.get('token') == app.config['FLASK_TOKEN']:
             if request.method == "POST":
-                body = request.get_json(force=True) # I tested printing the content headers and wasn't
-                                                    # able to get the Content-Type: application/json from Fabman, so we force parsing as json here
+                body = request.get_json(force=True) # need force...
                 try:
                     if body['type'] == 'resourceLog_created' and body['details']['resource']['id'] == app.config['FABMAN_RESOURCE_ID']:
                         log_type = body['details']['log']['type']
@@ -49,25 +48,6 @@ def parse_activity_webhook(request):
 # SOCKET.IO
 
 socketio = SocketIO(app)
-# @socketio.on('message')
-# def handleMessage(msg):
-# 	print('Message: ' + msg)
-# 	send(msg, broadcast=True)
-
-# @socketio.on('my event')
-# def handleMessage(msg):
-# 	# print('Message: ' + msg)
-# 	emit('my response', {'data': msg['data']})
-
-# @socketio.on('connect')
-# def test_connect():
-#     print('connected')
-    # send('server successfully connected to client!')
-
-# @socketio.on('member check-in')
-# def handleMessage(data):
-#     emit('member check-in', {'data': data})
-
 # FLASK
 
 @app.route("/webhook", methods=["POST"])
