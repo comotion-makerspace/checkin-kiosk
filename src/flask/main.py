2898146409376431485d9fb4e2f50db995cf401d
codycodes <codycodes@users.noreply.github.com>
2019-07-19 02:18:41 -0700

    :recycle: env vars from single location now

--- src/flask/main.py
+++ src/flask/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 import requests
+import os
 
 
 app = Flask(__name__)
-# TODO: make all envvars passed via docker-compose
-app.config.from_envvar('APP_SETTINGS')
+socketio = SocketIO(app)
+
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+FLASK_TOKEN = os.environ['FLASK_TOKEN']
+FABMAN_RESOURCE_ID = os.environ['FABMAN_RESOURCE_ID']
+STAFF_URL = os.environ['STAFF_URL']
+FLASK_PORT = os.environ['FLASK_PORT']
 
 def parse_activity_webhook(request):
     '''
@@ -15,11 +21,11 @@ def parse_activity_webhook(request):
     Returns a dict with the member's name and their status -> "allowed", "denied", or "keyAssigned"
     '''
     try:
-        if request.args.get('token') == app.config['FLASK_TOKEN']:
+        if request.args.get('token') == FLASK_TOKEN:
             if request.method == "POST":
                 body = request.get_json(force=True) # need force...
                 try:
-                    if body['type'] == 'resourceLog_created' and body['details']['resource']['id'] == app.config['FABMAN_RESOURCE_ID']:
+                    if body['type'] == 'resourceLog_created' and body['details']['resource']['id'] == int(FABMAN_RESOURCE_ID):
                         log_type = body['details']['log']['type']
                         if log_type in ('allowed', 'denied', 'keyAssigned'):
                             if body['details']['log']['member']:
@@ -35,33 +41,25 @@ def parse_activity_webhook(request):
                         name = body['details']['createdBy']['firstName']
                         return name, 'allowed'
                 except KeyError:
-                    print("ERROR IN FABMAN JSON RESPONSE please examine logs for further info")
+                    print("ERROR PARSING FABMAN JSON RESPONSE; please examine logs for further info")
                     return None
-                
         else:
-            # TODO: learn how to raise exceptions properly...
-            print("UNAUTHORIZED") # Check your token - is it present & valid?
+            print("UNAUTHORIZED REQUEST MADE") # Check your token - is it present & valid?
             return None
 
     except Exception as e:
         print(e)
 
-# SOCKET.IO
-
-socketio = SocketIO(app)
-# FLASK
-
 @app.route("/webhook", methods=["POST"])
 def get_member_access():
     member_data = parse_activity_webhook(request)
-    print('received some data...{}'.format(member_data) )
+    print('received some data: {}'.format(member_data) )
     if member_data:
         emit('member check-in', {'data': member_data[:2]}, namespace='/', broadcast=True)
         requests.post('{}/staff?token={}'
-                    .format(app.config['STAFF_URL'],
-                    app.config['FLASK_TOKEN']),
+                    .format(STAFF_URL, FLASK_TOKEN),
                     json={'data': member_data[-1]})
     return 'done', 200
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0', port=FLASK_PORT)
